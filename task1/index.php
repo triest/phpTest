bf38de3da91182490e601a8e1cb6a9942e631a9b
triest <you@example.com>
2019-10-19 17:15:25 +0300

    Add auth by session

--- task1/index.php
+++ task1/index.php
@@ -11,18 +11,6 @@ User::connect();
 
 
 $user = new User();
-/*
-$user->name = "test";
-$user->password = "test";
-$user->save();
-*/
-
-
-//$user = User::getUserByName("test2");
-//var_dump($user);
-//$user->deleteUser();
-
-//$array_users = User::getAll();
 
 ($user->login("test2","test2"));
 echo "auth_user: ".$_SESSION["auth_user"];
