96e4c8a6fdb8d36ea4bdf4edd69661d98333679b
triest <you@example.com>
2019-10-20 16:55:37 +0300

    Add change fixus on delete

--- task2/js/index.js
+++ task2/js/index.js
@@ -1,26 +1,40 @@
 var part1;
 var part2;
+var code;
 window.onload = function () {
     part1 = document.getElementById('part1');
     part2 = document.getElementById('part2');
+    code = document.getElementById('code');
+    part2.oninput = function () {
+        if (part2.value.length == 0) {
+            part1.focus();
+        }
+    };
+    part1.oninput = function () {
+        if (part1.value.length == 0) {
+            code.focus();
+        }
+    };
 };
 
+
 function countChar1() {
-    let input = document.getElementById('code').value;
+    let input = code.value;
     if (input.length >= 2) {
         part1.focus();
     }
 }
 
 function countChar2() {
-    let input = document.getElementById('part1').value;
+    let input = part1.value;
     if (input.length >= 2) {
         part2.focus();
     }
 }
 
 function countChar3() {
-    let input = document.getElementById('part2').value;
+    let input = part2.value;
+    console.log(input.length);
     if (input.length === 3) {
         sendRequwest()
     }
@@ -30,18 +44,21 @@ function sendRequwest() {
     var formData = {s: 's'};
     var xhr = new XMLHttpRequest();
     var data = {};
-    data.code = document.getElementById('code').value;
-    data.part1 = document.getElementById('part1').value;
-    data.part2 = document.getElementById('part2').value;
+    data.code = code.value;
+    data.part1 = part1.value;
+    data.part2 = part2.value;
+    if (data.code == "" || data.part2 == "") {
+        part2.focus();
+    }
     $.ajax({
         type: 'POST',
         url: './server.php',
         data,
         success: function (msg) {
             alert(msg);
-            document.getElementById('code').disabled = true;
-            document.getElementById('part1').disabled = true;
-            document.getElementById('part2').disabled = true
+            code.style.display = 'none';
+            part1.style.display = 'none';
+            part2.style.display = 'none';
         }
     });
 
